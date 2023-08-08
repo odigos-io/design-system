aa8af7fdb79c4a8cba163ecc9a33d8719cf0e45a
alonkeyval <alon@keyval.dev>
2023-08-08 13:27:00 +0300

    remove unude files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: false,
+  images: {
+    unoptimized: true,
+  },
+  output: "export",
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: {
+        loader: "@svgr/webpack",
+        options: {
+          svgoConfig: {
+            plugins: [
+              {
+                name: "removeViewBox",
+                active: false,
+              },
+            ],
+          },
+        },
+      },
+    });
+
+    return config;
+  },
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,3 @@
-"use client";
-import {
-  KeyvalButton,
-  KeyvalCard,
-  KeyvalCheckbox,
-  KeyvalText,
-} from "@/design.system";
-import Checked from "@/assets/icons/checkbox-rect.svg";
 export default function Home() {
-  return (
-    <main>
-      <KeyvalText color="#000" size={14} weight={600}>
-        Hello World
-      </KeyvalText>
-      <KeyvalButton>
-        <KeyvalText color="#000" size={14} weight={600}>
-          Hello World
-        </KeyvalText>
-      </KeyvalButton>
-      <KeyvalCard>
-        <KeyvalText color="#000" size={14} weight={600}>
-          Hello World
-        </KeyvalText>
-      </KeyvalCard>
-      <KeyvalCheckbox value={false} onChange={() => {}} label={""} />
-      {/* <Checked /> */}
-    </main>
-  );
+  return null;
 }
