a88a276d5c45213528ad098500bd736b56e4d48c
alonkeyval <alon@keyval.dev>
2023-09-13 16:37:22 +0300

    feat(card-component): add body option to header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
 'use client';
-
+import React from 'react';
+import { Card } from '../design.system/card/card';
+import { Text } from '../design.system/text/text';
 export default function Home() {
-  return <div style={{ background: '#0E1C28' }}></div>;
+  return (
+    <div style={{ background: '#0E1C28' }}>
+      <Card
+        type={'secondary'}
+        header={{
+          body: () => (
+            <div
+              style={{
+                width: 600,
+                display: 'flex',
+                justifyContent: 'space-between',
+              }}
+            ></div>
+          ),
+        }}
+      >
+        <div style={{ width: 600, height: 600 }}></div>
+      </Card>
+    </div>
+  );
 }
