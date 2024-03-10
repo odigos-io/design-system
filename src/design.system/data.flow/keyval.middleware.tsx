c77580aeaa3dd483cc1ed527b6b483793c0ff0bb
alonkeyval <alon@keyval.dev>
2024-03-10 16:51:06 +0200

    chore: change odigos center node

--- src/design.system/data.flow/keyval.middleware.tsx
+++ src/design.system/data.flow/keyval.middleware.tsx
@@ -1,23 +1,67 @@
-import React, { memo } from "react";
-import { Handle, Position } from "reactflow";
-import { KeyvalMiddleware } from "@/assets/icons/overview";
+import React from 'react';
+import { Handle, Position } from 'reactflow';
+import { Logo } from '@/assets';
+import Image from 'next/image';
 
-export default function NamespaceNode({ isConnectable }: any) {
+export default function OdigosCenterNode({ isConnectable }: any) {
   return (
-    <div>
-      <Handle
-        type="target"
-        position={Position.Left}
-        style={{ visibility: "hidden" }}
-      />
-      <KeyvalMiddleware />
-      <Handle
-        type="source"
-        position={Position.Right}
-        id="a"
-        isConnectable={isConnectable}
-        style={{ visibility: "hidden" }}
-      />
+    <div
+      style={{
+        width: 120,
+        height: 120,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 60,
+        border: 'solid 1px #3a3a3a11',
+        background: '#110c1f55',
+      }}
+    >
+      <div
+        style={{
+          width: 100,
+          height: 100,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: 50,
+          border: 'solid 1px #3a3a3a76',
+          background: '#110c1f7d',
+        }}
+      >
+        <div
+          style={{
+            width: 80,
+            height: 80,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: 50,
+            border: 'solid 1px #3a3a3aa9',
+            background: '#110c1f',
+          }}
+        >
+          <Handle
+            type="target"
+            position={Position.Left}
+            style={{ visibility: 'hidden' }}
+          />
+          <Image
+            src={Logo}
+            alt="logo"
+            width={64}
+            height={64}
+            style={{ borderRadius: 100 }}
+          />
+          <Handle
+            type="source"
+            position={Position.Right}
+            id="a"
+            isConnectable={isConnectable}
+            style={{ visibility: 'hidden' }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
