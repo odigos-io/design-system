c097df4fb114b5969ba919c353e724c2ff1a4ec0
Alon Braymok <138359965+alonkeyval@users.noreply.github.com>
2024-07-21 14:36:37 +0300

    chore: export icons (#23)

--- src/icons-components/actions/index.tsx
+++ src/icons-components/actions/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import { AddClusterInfo, DeleteAttribute, RenameAttribute } from '@/assets';
+import {
+  AddClusterInfo,
+  DeleteAttribute,
+  ErrorSampler,
+  LatencySampler,
+  PiiMasking,
+  RenameAttribute,
+  ProbabilisticSampler,
+} from '@/assets';
 import { HomeIconProps } from '..';
 import { IconWrapper } from '../styled';
 
@@ -18,5 +26,29 @@ export const DeleteAttributeIcon = ({ ...props }: HomeIconProps) => (
 export const RenameAttributeIcon = ({ ...props }: HomeIconProps) => (
   <IconWrapper {...props}>
     <RenameAttribute />
+  </IconWrapper>
+);
+
+export const ErrorSamplerIcon = ({ ...props }: HomeIconProps) => (
+  <IconWrapper {...props}>
+    <ErrorSampler />
+  </IconWrapper>
+);
+
+export const PiiMaskingIcon = ({ ...props }: HomeIconProps) => (
+  <IconWrapper {...props}>
+    <PiiMasking />
+  </IconWrapper>
+);
+
+export const LatencySamplerIcon = ({ ...props }: HomeIconProps) => (
+  <IconWrapper {...props}>
+    <LatencySampler />
+  </IconWrapper>
+);
+
+export const ProbabilisticSamplerIcon = ({ ...props }: HomeIconProps) => (
+  <IconWrapper {...props}>
+    <ProbabilisticSampler />
   </IconWrapper>
 );
